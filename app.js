bdaedbf73780cc406286666b404e5bdd826a0a9e
Sorakatop <alcocergonzalomatias@gmail.com>
2021-06-11 09:31:37 -0300

    Corregido listen

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 3030
 
-app.listen(3030,()=>console.log('Servidor corriendo'));
+app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}` ))
+
 app.use(express.static('public'));
 
 app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views/home.html")));
